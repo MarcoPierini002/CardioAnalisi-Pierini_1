2f87a279776a2bc0f84c41d53dc0e756c39f3c94
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-04-04 09:54:38 +0200

    Add project files.

---
+++ CardioLibrary/DataCardio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardioLibrary
+{
+    public class DataCardio
+    {
+        public static void Metodo_1()
+        {
+            int frequenzamassima = 0;
+            int eta = 0;
+            Console.WriteLine("inerisci l'eta");
+            eta = Convert.ToInt32(Console.ReadLine());
+            frequenzamassima = eta - 220;
+
+        }
+            
+        
+    }
+}
